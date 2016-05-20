015382d51e3040f5293fb7b2ce3c4578686cf9ea
bsg15 <benjamin.gunton15@imperial.ac.uk>
2016-05-20 15:46:18 +0100

    Built up Skeleton
    - Added structs for processor and decoded arguments
    - Designed program workflow using comments
    - Created loop body for Fetch decode Execute Cycle and created a loop counter
    
    Contributions from entire group

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,65 @@
 #include <stdlib.h>
+#include <limits.h>
+#include <stdint.h>
+
+#define NUMBER_OF_REGISTERS   17
+#define BYTES_IN_MEMORY       65536
+// index of Program Counter in registers array
+#define PC                    15
+// index of CPSR in registers array
+#define CPSR                  16
+
+struct processor {
+  uint32_t registers[NUMBER_OF_REGISTERS];	 
+  uint8_t memory[BYTES_IN_MEMORY];
+  bool end;
+  int counter = 0; // counter to detewrmine whether to decode and execute
+}
+
+struct arguments {
+  uint8_t dRegIndex;
+  uint8_t sRegIndex;
+  uint8_t nRegIndex;
+  uint8_t mRegIndex;
+  uint16_t operand2;
+  uint8_t cond;
+  uint32_t offset;
+  void *executePointer(struct arguments args) = NULL; 
+}
+
 
 int main(int argc, char **argv) {
+  struct processor arm;
+  struct arguments decodedArgs;
+
+  // load program from args and initialise processor and read from file
+
+
+  // points to appropriate execute function after decoding
+  uint32_t dInstruction; // instruction to be decoded next
+
+  while (!arm.end) {
+    
+    // execute instruction
+    if (arm.counter >= 2) {
+      execute(decodedArgs, arm);
+    }
+
+    // decode instruction
+    if (arm.counter >= 1) {
+      decode(dInstruction, decodedArgs);
+    }
+
+    // fetch instruction
+    dInstruction = fetch(dInstruction, arm);
+
+    // Increment counter but avoid overflow of counter
+    if (counter < 3){
+      arm.counter++;
+    }
+  }
+
+  // print register states
+
   return EXIT_SUCCESS;
 }
