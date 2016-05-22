ae3bfba5de23a393c9a6653f1d4c1e59c838a0c0
Uno <hperwironegoro@gmail.com>
2016-05-22 18:36:05 +0100

    Organised the header file.

--- src/emulate.h
+++ src/emulate.h
@@ -1,15 +1,29 @@
 #ifndef EMULATE_H
 #define EMULATE_H
+
+// Structs
 struct processor;
 struct arguments;
 
+// Main flow
+uint32_t fetch(struct processor arm);
+
+// Data Processing
+
+// Branching
+
+// Multiply
+
+// Single Data Transfter
+
+// Helper functions
 uint32_t setBit(uint32_t word, bool set, uint8_t position);
-uint32_t rotateRight32(uint32_t val, uint16_t n);
-uint32_t arithShiftRight32(uint32_t val, uint16_t n);
+
 void resolveOperand2(uint16_t op, bool iFlag, struct arguments *decodedArgs,
         struct processor *arm);
+uint32_t rotateRight32(uint32_t val, uint16_t n);
+uint32_t arithShiftRight32(uint32_t val, uint16_t n);
 uint32_t shift(uint8_t shiftCode, uint32_t value, uint16_t,
         struct processor *arm, bool sFlag);
-uint32_t fetch(struct processor arm);
 
 #endif
