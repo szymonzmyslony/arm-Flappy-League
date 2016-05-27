d179b24952a54b374223a36d7608af934311a63b
Harjuno Perwironegoro <hp1515@matrix33.doc.ic.ac.uk>
2016-05-27 17:33:12 +0100

    Added the main unfinished framework for the passes

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,46 @@
-#include <stdlib.h>
+#include "assemble.h"
 
 int main(int argc, char **argv) {
+
+  readfile(); //TODO Finish this
+  
+  // We may assume that no line is longer than 511 characters;
+  char *line = calloc(MAX_LINE_LENGTH + 1, sizeof(char));
+  checkAllocError(line, (MAX_LINE_LENGTH + 1) * sizeof(char));
+  
+  // -- First pass
+  
+  for(int i = 0; file[i]; i++) {
+    
+    // End of line reached
+    if(file[i] == '\n') {
+      line[cIndex] = "\0";
+      
+      cIndex = 0;
+    } else {
+      line[cIndex] = file[i];
+    }
+    
+    cIndex++;
+  }
+  
+  // -- Second pass
+  // The index of the current character in the line
+  int cIndex = 0;
+  
+  for(int i = 0; file[i]; i++) {
+    
+    // End of line reached
+    if(file[i] == '\n') {
+      line[cIndex] = "\0";
+      
+      cIndex = 0;
+    } else {
+      line[cIndex] = file[i];
+    }
+    
+    cIndex++;
+  }
+  
   return EXIT_SUCCESS;
 }
