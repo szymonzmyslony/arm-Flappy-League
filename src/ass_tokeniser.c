4b89d7b24eedec5738c58d8597f7374041415a74
Uno <hperwironegoro@gmail.com>
2016-05-28 18:39:34 +0100

    Finished the tokeniser

--- src/ass_tokeniser.c
+++ src/ass_tokeniser.c
@@ -1,18 +1,20 @@
 #include "ass_tokeniser.h"
 
 // Breaks a line into its label, opcode and operand fields.
-// Modifies the parameters *label, *opcode and **opFields.
-void tokenise (char *line, char *label, char *opcode, char **opFields) {
-  char *saveptr; //TODO init
+// Modifies the arguments *label, *opcode and **opFields.
+void tokenise (char *line, char *label, char *opcode, char **opFields) {  
+  // Set the label, if it exists
+  strcpy(label, strtok(line, ":"));
   
-  label = strtok_r(line, ":", &saveptr);
+  // Set the opcode
+  strcpy(opcode, strtok(NULL, " "));
   
-  opcode = strtok_r(NULL, " ", &saveptr);
-  
-  char *opField = //TODO ALL OF THIS
-  opField = strtok_r(NULL, ",", &saveptr);
-  
-  while(opField != NULL) {
-    opField = strtok_r(NULL, ",", &saveptr);
-  }
+  // Set the opFields
+  int index = 0;
+  char opField[MAX_OPFIELD_SIZE];
+  do {
+    strcpy(opField, strtok(NULL, ","));
+    strcpy(opFields[index], opField);
+    index++;
+  } while(opField != NULL);
 }
--- src/ass_tokeniser.h
+++ src/ass_tokeniser.h
@@ -1,6 +1,7 @@
 #ifndef ASS_TOKENISER_H
 #define ASS_TOKENISER_H
 #include <string.h>
+#define MAX_OPFIELD_SIZE 11;
 
 tokenise (char *line, char *label, char *opcode, char **opFields);
 
