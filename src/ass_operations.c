f28cef9db91d4cb4ab9ff2c5b9394f94b7d07494
Benjamin Gunton <benjamin.guntonn15@ic.ac.uk>
2016-05-29 15:22:21 +0100

    Created very simple skeleton for loading and storing encoding functions

---
+++ src/ass_operations.c
@@ -0,0 +1,19 @@
+#include "helperFunctions.h"
+#include "emulate.h"
+
+uint32_t encodeSDTldr (char **opfields) {
+  uint32_t binInstruction;
+  // initialise binInstruction
+
+  // resolve all opFields
+
+  // set IPUL bits
+
+  // add value to end of file if necessary
+
+  // return
+} 
+
+uint32_t encodeSDTstr (char **opfields) {
+  uint32_t binInstruction
+} 
