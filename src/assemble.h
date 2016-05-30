a2b0bcf58eeb1968843600440916c844a878d4c1
Uno <hperwironegoro@gmail.com>
2016-05-31 00:19:24 +0100

    Added more macros, inclusions and a function declaration.
    - Included the symbol table and tokeniser headers
    - Added macros for the opCode and opFields' max sizes
    - Added the declaration for getMemAddr

--- src/assemble.h
+++ src/assemble.h
@@ -3,7 +3,14 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include "helperFunctions.h"
+#include "ass_symbols.h"
+#include "ass_tokeniser.h"
 
-#define MAX_LINE_LENGTH 511
+#define MAX_LINE_LENGTH     511
+//andeq is largest opcode
+#define MAX_OPCODE_LENGTH     6
+#define MAX_OPFIELD_SIZE      4 //TODO check this
+
+uint32_t getMemAddr(void);
 
 #endif
