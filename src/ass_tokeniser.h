9ffb553f471d73277c11f652772de4b7cb9cb31c
Uno <hperwironegoro@gmail.com>
2016-05-31 00:16:12 +0100

    Changed a macro name for consistency, changed opcode to opCode.

--- src/ass_tokeniser.h
+++ src/ass_tokeniser.h
@@ -1,9 +1,9 @@
 #ifndef ASS_TOKENISER_H
 #define ASS_TOKENISER_H
 #include <string.h>
-#include <helperFunctions.h>
-#define MAX_OPFIELD_SIZE 20;
+#include "helperFunctions.h"
+#define MAX_OPFIELD_LENGTH 20;
 
-tokenise (char *line, char *label, char *opcode, char **opFields);
+tokenise (char *line, char *label, char *opCode, char **opFields);
 
 #endif
