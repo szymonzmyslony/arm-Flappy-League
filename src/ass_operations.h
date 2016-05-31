b355f95243f197b263429e5fe59bcc300f7fd88e
sz6315 <Mrsz0204>
2016-05-31 11:35:16 +0100

    modified the header file - added funtion declarations

---
+++ src/ass_operations.h
@@ -0,0 +1,9 @@
+uint32_t encodeMul (char **opfields);
+uint32_t encodeMla (char **opfields) ;
+uint32_t encodeBeq (char **opfields);
+uint32_t encodeBne (char **opfields);
+uint32_t encodeBge (char **opfields);
+uint32_t encodeBlt (char **opfields);
+uint32_t encodeBgt (char **opfields);
+uint32_t encodeBle (char **opfields);
+uint32_t encodeBal (char **opfields);
