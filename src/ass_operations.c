faf95b3454d8491647e31ab5bf1f754427b928f7
sz6315 <Mrsz0204>
2016-05-31 11:59:21 +0200

    basic frame for branching encoding creates

--- src/ass_operations.c
+++ src/ass_operations.c
@@ -9,7 +9,7 @@ uint32_t encodeMul (char **opfields) {
   char *operand = opFields[opFieldsIndex];
   
   //Set Rd
-  removeLeadingSpace(operand);
+  trim(operand);
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex << 12;
   binInstruction |= regIndex32;
@@ -17,14 +17,14 @@ uint32_t encodeMul (char **opfields) {
   
   
   //Set Rm
-  removeLeadingSpace(operand);
+  trim(operand);
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex;
   binInstruction |= regIndex32;
   opFieldsIndex++;
   
   //set Rs
-  removeLeadingSpace(operand);
+  trim(operand);
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex<<4; 
   binInstruction |= regIndex32;
@@ -39,7 +39,7 @@ uint32_t encodeMul (char **opfields) {
   char *operand = opFields[opFieldsIndex];
   
   //Set Rd
-  removeLeadingSpace(operand);
+  trim(operand);
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex << 12;
   binInstruction |= regIndex32;
@@ -47,21 +47,21 @@ uint32_t encodeMul (char **opfields) {
   
   
   //Set Rm
-  removeLeadingSpace(operand);
+  trim(operand);
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex;
   binInstruction |= regIndex32;
   opFieldsIndex++;
   
   //set Rs
-  removeLeadingSpace(operand);
+  trim(operand);
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex<<4; 
   binInstruction |= regIndex32;
    opFieldsIndex++;
    
    //set Rn
-   removeLeadingSpace(operand);
+  trim(operand);
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex<<8; 
   binInstruction |= regIndex32;
@@ -71,18 +71,91 @@ uint32_t encodeMul (char **opfields) {
 }
 
  uint32_t encodeBeq (char **opfields) {
-  // Cond is 0000
-  uint32_t binInstruction = 0xA000000;
-  
+	  // Cond is 0000
+  uint32_t binInstruction = 0xa000000;
+  uint32_t current =  getMemAddr();
+
   int opFieldsIndex = 0;
   char *operand = opFields[opFieldsIndex];
+  trim(operand);
+  //get Label adress
+  uint32_t labelAdress = getValue(struct, operand); 
+	 
+ 
+  //account for pipeline
+  current=current+8;  
+  int offset = labelAdress-current;	  
+  offset=offset>>2;  
+  binInstruction |= offset;
   
-
-  removeLeadingSpace(operand);
-  //get label adress
+  
+  
+  
+  
+  
   
   
 }
+
+uint32_t encodeBne (char **opfields){
+	  // Cond is 0001
+	uint32_t binInstruction = 0x1a000000;
+	
+	
+	
+	
+}
+
+
+
+
+uint32_t encodeBge (char **opfields){
+	  // Cond is 1010
+	uint32_t binInstruction = 0xAA000000;
+	
+	
+	
+}
+
+uint32_t encodeBlt (char **opfields){
+	  // Cond is 1011
+	uint32_t binInstruction = 0xBA000000;
+	
+	
+	
+}
+
+
+uint32_t encodeBgt (char **opfields){
+	  // Cond is 1100
+	uint32_t binInstruction = 0xCA000000;
+	
+	
+	
+}
+
+uint32_t encodeBle (char **opfields){
+	  // Cond is 1101
+	uint32_t binInstruction = 0xDA000000;
+	
+	
+	
+}
+
+uint32_t encodeBal (char **opfields){
+	  // Cond is 1110
+	
+	uint32_t binInstruction = 0xEA000000;
+	
+	
+	
+	
+	
+	
+	
+}
+
+
 
 
   
