6905de6425d41445633e14da977948449dcaa0a0
sz6315 <Mrsz0204>
2016-05-31 14:26:16 +0100

    included funtions from other files, cleaned whitespaces

--- src/ass_operations.c
+++ src/ass_operations.c
@@ -1,105 +1,80 @@
 #include "helperFunctions.h"
 #include "ass_operations.h"
+#include "emulate.h"
+#include "ass_symbols.h"
+#include "assemble.h"
 
 #define ENC_BRANCHING(name, condition)             \
-uint32_t name(char **opFields){ 					\			   
-	uint32_t binInstruction = 0xA000000;           \
-	                                              
-	binInstruction |= condition << 28;             \
-	
-	
-   uint32_t current =  getMemAddr(); \
-   int opFieldsIndex = 0; \
-   char *operand = opFields[opFieldsIndex]; \
-
-   //get Label adress
-   uint32_t labelAdress = getValFromStruct(&symbolsTable, operand); 	 \
-   //account for pipeline 
-   current=current+8;  \
-   int offset = labelAdress-current;	  \
-	
-   offset=offset>>2;  \
-   
-   binInstruction |= offset; 
-		
-	return binInstruction;                         \
+uint32_t name(char **opFields){ 				   \			   
+  uint32_t binInstruction = 0xA000000;           \	                                              
+  binInstruction |= condition << 28;             \	
+  uint32_t current =  getMemAddr(); \
+  int opFieldsIndex = 0; \
+  char *operand = opFields[opFieldsIndex]; \
+  uint32_t labelAdress = getValFromStruct(&symbolsTable, operand); 	 \
+  current=current+8;  \
+  int offset = labelAdress-current;	  \	
+  offset=offset>>2;  \   
+  binInstruction |= offset; 
+  return binInstruction;                         \
 }
+
 ENC_BRANCHING(encodeBeq, COND_eq );
+
 ENC_BRANCHING(encodeBne, COND_ne );
+
 ENC_BRANCHING(encodeBge, COND_ge );
+
 ENC_BRANCHING(encodeBlt, COND_lt);
+
 ENC_BRANCHING(encodeBgt, COND_gt);
+
 ENC_BRANCHING(encodeBle, COND_le);
-ENC_BRANCHING(encodeBal,  COND_al);
 
-     
-
-
-
-
-
+ENC_BRANCHING(encodeBal, COND_al);
 
 uint32_t encodeMul (char **opfields) {
   // Cond is 1110, A is 0 and S is 0;
   uint32_t binInstruction = 0xC0000090;
   int opFieldsIndex = 0;
-
-  char *operand = opFields[opFieldsIndex];
-  
-  //Set Rd
-  
+  char *operand = opFields[opFieldsIndex];  
+  //Set Rd  
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex << 12;
   binInstruction |= regIndex32;
-  opFieldsIndex++;
-  
-  
-  //Set Rm
-  
+  opFieldsIndex++;  
+  //Set Rm  
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex;
   binInstruction |= regIndex32;
-  opFieldsIndex++;
-  
-  //set Rs
- 
+  opFieldsIndex++;  
+  //set Rs 
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex<<4; 
-  binInstruction |= regIndex32;
-  
+  binInstruction |= regIndex32;  
 }
-  uint32_t encodeMla (char **opfields) {
+
+uint32_t encodeMla (char **opfields) {
   // Cond is 1110, A is 1 and S is 0;
   uint32_t binInstruction = 0xC0200090;
   int opFieldsIndex = 0;
-
-
-  char *operand = opFields[opFieldsIndex];
-  
+  char *operand = opFields[opFieldsIndex];  
   //Set Rd
-
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex << 12;
   binInstruction |= regIndex32;
-  opFieldsIndex++;
-  
-  
-  //Set Rm
- 
+  opFieldsIndex++;   
+  //Set Rm 
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex;
   binInstruction |= regIndex32;
-  opFieldsIndex++;
-  
-  //set Rs
- 
+  opFieldsIndex++;  
+  //set Rs 
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex<<4; 
   binInstruction |= regIndex32;
-   opFieldsIndex++;
-   
-   //set Rn
- 
+  opFieldsIndex++;   
+  //set Rn 
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex<<8; 
   binInstruction |= regIndex32;
