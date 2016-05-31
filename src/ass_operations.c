026547e37920f1eae5ea70ce5bd30853f484f48a
sz6315 <Mrsz0204>
2016-05-31 14:19:39 +0100

    macros is working, other funtions deleted from branching, got rid off trimming

--- src/ass_operations.c
+++ src/ass_operations.c
@@ -1,20 +1,43 @@
 #include "helperFunctions.h"
 #include "ass_operations.h"
 
-int getOffset(char **opfields){
-  uint32_t current =  getMemAddr(); \
-  int opFieldsIndex = 0; \
-  char *operand = opFields[opFieldsIndex]; \
-  trim(operand); \
-  //get Label adress
-  uint32_t labelAdress = getValFromStruct(&symbolsTable, operand); 	 \
-  //account for pipeline 
-  current=current+8;  \
-  int offset = labelAdress-current;	  \
-  offset=offset>>2;  \
-  return offset; \	
+#define ENC_BRANCHING(name, condition)             \
+uint32_t name(char **opFields){ 					\			   
+	uint32_t binInstruction = 0xA000000;           \
+	                                              
+	binInstruction |= condition << 28;             \
 	
+	
+   uint32_t current =  getMemAddr(); \
+   int opFieldsIndex = 0; \
+   char *operand = opFields[opFieldsIndex]; \
+
+   //get Label adress
+   uint32_t labelAdress = getValFromStruct(&symbolsTable, operand); 	 \
+   //account for pipeline 
+   current=current+8;  \
+   int offset = labelAdress-current;	  \
+	
+   offset=offset>>2;  \
+   
+   binInstruction |= offset; 
+		
+	return binInstruction;                         \
 }
+ENC_BRANCHING(encodeBeq, COND_eq );
+ENC_BRANCHING(encodeBne, COND_ne );
+ENC_BRANCHING(encodeBge, COND_ge );
+ENC_BRANCHING(encodeBlt, COND_lt);
+ENC_BRANCHING(encodeBgt, COND_gt);
+ENC_BRANCHING(encodeBle, COND_le);
+ENC_BRANCHING(encodeBal,  COND_al);
+
+     
+
+
+
+
+
 
 uint32_t encodeMul (char **opfields) {
   // Cond is 1110, A is 0 and S is 0;
@@ -24,7 +47,7 @@ uint32_t encodeMul (char **opfields) {
   char *operand = opFields[opFieldsIndex];
   
   //Set Rd
-  trim(operand);
+  
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex << 12;
   binInstruction |= regIndex32;
@@ -32,14 +55,14 @@ uint32_t encodeMul (char **opfields) {
   
   
   //Set Rm
-  trim(operand);
+  
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex;
   binInstruction |= regIndex32;
   opFieldsIndex++;
   
   //set Rs
-  trim(operand);
+ 
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex<<4; 
   binInstruction |= regIndex32;
@@ -54,7 +77,7 @@ uint32_t encodeMul (char **opfields) {
   char *operand = opFields[opFieldsIndex];
   
   //Set Rd
-  trim(operand);
+
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex << 12;
   binInstruction |= regIndex32;
@@ -62,137 +85,27 @@ uint32_t encodeMul (char **opfields) {
   
   
   //Set Rm
-  trim(operand);
+ 
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex;
   binInstruction |= regIndex32;
   opFieldsIndex++;
   
   //set Rs
-  trim(operand);
+ 
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex<<4; 
   binInstruction |= regIndex32;
    opFieldsIndex++;
    
    //set Rn
-  trim(operand);
+ 
   uint8_t regIndex = getRegIndex(operand);
   uint32_t regIndex32 = regIndex<<8; 
   binInstruction |= regIndex32;
    
    
   
-}
-
- uint32_t encodeBeq (char **opfields) {
-	  // Cond is 0000
-  uint32_t binInstruction = 0xa000000;   
-  binInstruction |= OFFSET(&opFields);
-  
- 
-  
-  
-  
-  
-}
-
-uint32_t encodeBne (char **opfields){
-	  // Cond is 0001
-	uint32_t binInstruction = 0x1a000000;
-		
-	binInstruction |= OFFSET;
-  
-	
-	
-	
-	
-}
-
-
-
-
-uint32_t encodeBge (char **opfields){
-	  // Cond is 1010
-	uint32_t binInstruction = 0xAA000000;
-	
-	
-	 binInstruction |= OFFSET;
-  
-	
-	
-	
-}
-
-uint32_t encodeBlt (char **opfields){
-	  // Cond is 1011
-	uint32_t binInstruction = 0xBA000000;
-	 binInstruction |= OFFSET;
-	
-	
-	
-}
-
-
-uint32_t encodeBgt (char **opfields){
-	  // Cond is 1100
-	uint32_t binInstruction = 0xCA000000;
-	
-	
-	 binInstruction |= OFFSET;
-	
-	
-	
-}
-
-uint32_t encodeBle (char **opfields){
-	  // Cond is 1101
-	uint32_t binInstruction = 0xDA000000;
-	
-	
-	int opFieldsIndex = 0;
-  char *operand = opFields[opFieldsIndex];
-  trim(operand);
-  //get Label adress
-  uint32_t labelAdress = getValFromStruct(&symbolsTable, operand); 	 
- 
-  //account for pipeline
-  current=current+8;  
-  int offset = labelAdress-current;	  
-  offset=offset>>2;  
-  binInstruction |= offset;
-  
-	
-	
-	
-}
-
-uint32_t encodeBal (char **opfields){
-	  // Cond is 1110
-	
-	uint32_t binInstruction = 0xEA000000;
-	
-	
-	
-	int opFieldsIndex = 0;
-  char *operand = opFields[opFieldsIndex];
-  trim(operand);
-  //get Label adress
-  uint32_t labelAdress = getValFromStruct(&symbolsTable, operand); 	 
- 
-  //account for pipeline
-  current=current+8;  
-  int offset = labelAdress-current;	  
-  offset=offset>>2;  
-  binInstruction |= offset;
-  
-	
-	
-	
-	
-	
-	
-	
 }
 
 
