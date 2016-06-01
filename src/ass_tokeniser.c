a6a279571fcae89a0d21f61df5d668bfe12c34b5
Uno <hperwironegoro@gmail.com>
2016-06-01 16:15:09 +0100

    Fixed the tokeniser.

--- src/ass_tokeniser.c
+++ src/ass_tokeniser.c
@@ -2,15 +2,17 @@
 
 // Breaks a line into its label, opCode and operand fields.
 // Modifies all arguments.
-void tokenise (char *line, char *opCode, char **opFields) {
-  // Set the opCode
-  strcpy(opCode, trim(strtok(NULL, " ")));
+void tokenise (char *line, char *opcode, char **opFields) {
+  char *token = strtok(line, " ");
+  trim(token);
+  // Set the opcode
+  strcpy(opcode, token);
 
   // Set the opFields
-  char opField[MAX_OPFIELD_LENGTH];
-  strcpy(opField, strtok(NULL, ","));
-  for(int i = 0; opField != NULL; i++) {
-	  strcpy(opFields[i], trim(opField));
-	  strcpy(opField, strtok(NULL, ","));
+  token = strtok(NULL, ",");
+  for(int i = 0; token != NULL; i++) {
+  	  trim(token);
+	  strcpy(opFields[i], token);
+	  token = strtok(NULL, ",");
   }
 }
