d0320cf95346b43210bcbe82d31325d1cfb61e66
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-07 21:28:25 +0100

    Made gObjs global and added some temporary test code.

--- src/extension/Tminus.c
+++ src/extension/Tminus.c
@@ -2,40 +2,15 @@
 #include <SDL/SDL_image.h>
 #include <stdbool.h>
 #include "Tminus.h"
+#include "CircleObj.h"
 
 enum Display { WINDOW_WIDTH = 1024, WINDOW_HEIGHT = 720 };
 enum Game { MAX_OBJECTS = 20 };
 
 //TODO consider SDL 2.0
 
-/** A union that represents a generic variable, allowing the GameObject struct
-* to be more versatile and extremely generic.
-*/
-typedef union var {
-  float f;
-  void *p;
-  bool b;
-  int i;
-} Var;
-
-/** A struct that represents every drawn object and every function that is run
-* in each frame. The variables are used in a way that is consistent to their
-* draw and update function pointers.
-*   For example, a countdown may use v1 as a float and v2 as the halfway point
-* to switch the sides of each player in a football game when update() is
-* called.
-*/
-typedef struct gameObject GameObject;
-struct gameObject {
-  Var v1, v2, v3, v4, v5, v6;
-  // The sprite drawn, if it is not NULL, and the function called to draw it
-  SDL_Surface *sprite;
-  void (*draw)(GameObject *gObj);
-  // A function called every frame, if it is not NULL
-  void (*update)(GameObject *gObj);
-};
-
 SDL_Surface *screen;
+GameObject **gObjs;
 
 /** SDL_main is used for Windows / Mac, we will just use Linux
  * DARWIN is MAC OS X
@@ -62,11 +37,13 @@ int main(int argc, char **argv) {
   SDL_Event event;
   bool running = true;
 
-  GameObject **gObjs = (GameObject**)calloc(MAX_OBJECTS, sizeof(GameObject*));
+  gObjs = (GameObject**)calloc(MAX_OBJECTS, sizeof(GameObject*));
   if(gObjs == NULL) {
     fprintf(stderr, "Error allocating memory\n");
     exit(EXIT_FAILURE);
   }
+  //TODO remove test code
+  gObjs[0] = initCircleObj(30, 400, 400, 20, -20);
 
   // Game Loop
   while(running) {
