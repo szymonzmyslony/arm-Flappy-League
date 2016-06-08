4cb7701d5fc8ad5850425da3fba3302652a98060
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-08 16:41:09 +0100

    Added function declarations.

--- src/extension/collisions.h
+++ src/extension/collisions.h
@@ -2,8 +2,16 @@
 #define COLLISIONS_H
 
 #include <stdlib.h>
-#include "Tminus.h"
+#include <stdbool.h>
+#include "Structs.h"
+
+enum colliderTypes {
+  COL_CIRCLE = 1,
+  COL_LINE   = 2,
+  COL_NET    = 3,
+};
 
 void resolveCollision(Vector *v1, Vector *v2, int m1, int m2, float e);
+void bool circlesCollided(GameObject *c1, GameObject *c2);
 
 #endif
