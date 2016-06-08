2af0618e264229208c412d831318c11168f640fa
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-08 01:56:29 +0100

    Added for drawing debug shapes.

---
+++ src/extension/Debug.h
@@ -0,0 +1,8 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+void debugDrawPixel(int x, int y, Uint32 pixel);
+void debugDrawCircle(int x, int y, int r);
+GameObject *initCircleObj(float r, float x, float y, float vx, float vy);
+
+#endif
