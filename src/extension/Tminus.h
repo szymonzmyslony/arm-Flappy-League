02ab2d63c1d1ea81e0bca43bedb37088d199a3bd
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-08 16:31:30 +0100

    Added function declarations.

--- src/extension/Tminus.h
+++ src/extension/Tminus.h
@@ -5,10 +5,15 @@
 #include "Structs.h"
 #include "CircleObj.h"
 #include "UtilObj.h"
+#include "collisions.h"
 
 SDL_Surface *getConsoleScreen(void);
 SDL_Surface *loadImage(char *path);
 void initSDL(void);
 void processKeyboardInput(SDL_Event *eventPtr, bool *running);
+
+void updateObjs(void);
+void handleCollisions(void);
+void drawSprites(void);
 
 #endif
