5ddca7b7587b2aa5064d5a374df9c7663dd499ca
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-09 02:07:22 +0100

    Added function declarations.

--- src/extension/Tminus.h
+++ src/extension/Tminus.h
@@ -2,6 +2,7 @@
 #define TMINUS_H
 
 #include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
 #include "Structs.h"
 #include "CircleObj.h"
 #include "UtilObj.h"
@@ -9,6 +10,9 @@
 
 SDL_Surface *getConsoleScreen(void);
 SDL_Surface *loadImage(char *path);
+Mix_Music *loadMusic(char *path);
+Mix_Chunk *loadSound(char *path);
+
 void initSDL(void);
 void processKeyboardInput(SDL_Event *eventPtr, bool *running);
 
