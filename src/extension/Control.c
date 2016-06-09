57d2365c5bf711536ebdfde0aa4672b6495e4ee7
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-09 04:15:49 +0100

    Fixed a typo.

--- src/extension/Control.c
+++ src/extension/Control.c
@@ -5,7 +5,7 @@ void moveLeft(GameObject *circObj) {
   circObj->v2.vec.y = -upVelocity;
 }
 
-void moveLeft(GameObject *circObj) {
+void moveRight(GameObject *circObj) {
   circObj->v2.vec.x = +sideVelocity;
   circObj->v2.vec.y = -upVelocity;
 }
