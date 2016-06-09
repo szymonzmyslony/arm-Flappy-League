efb73f0267f86de2b24f48ac018067c115464c3e
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-09 19:57:56 +0100

    Added function declarations, and size and position enums.

--- src/extension/GameEdit.h
+++ src/extension/GameEdit.h
@@ -1,11 +1,31 @@
 #ifndef GAMEEDIT_H
 #define GAMEEDIT_H
 #include <stdint.h>
+#include <stdbool.h>
 #include "Structs.h"
 #include "CircleObj.h"
+#include "SquareObj.h"
+#include "UtilObj.h"
+
+enum sizesAndPositions {
+  PLAYER_SIZE = 64,
+  BALL_SIZE = 64,
+  SCORE_WIDTH = 50,
+  SCORE_HEIGHT = 90,
+  SCORE_OFFSET_Y = WINDOW_HEIGHT / 4,
+  SCORE1_OFFSET_X = WINDOW_WIDTH / 2 + 100 - SCORE_WIDTH,
+  SCORE2_OFFSET_X = WINDOW_WIDTH / 2 - 100,
+  GOAL_OFFSET = WINDOW_HEIGHT / 4,
+  GOAL_WIDTH = WINDOW_WIDTH / 9,
+  GOAL_HEIGHT = WINDOW_HEIGHT / 3
+};
+
 
 void initMenu(void);
 void initGame(void);
 void initSetup(void);
+
+void scorePlayer1(GameObject *colObj);
+void scorePlayer2(GameObject *colObj);
 
 #endif
