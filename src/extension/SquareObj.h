5f939cac2889d7f75ee085bd88cf9bca6826feb2
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-09 20:01:11 +0100

    Added function declarations.

--- src/extension/SquareObj.h
+++ src/extension/SquareObj.h
@@ -1,9 +1,13 @@
 #ifndef NETOBJ_H
 #define NETOBJ_H
+#include <stdbool.h>
+#include "Structs.h"
 #include "UtilObj.h"
+#include "Debug.h"
 
 GameObject *initSquareObj(float x, float y, float w, float h, bool isSolid);
 void drawSquareObj(GameObject *squareObj);
 void setAsGoal(GameObject *scoreObj);
+void setCollFunc(GameObject *squareObj, void (*func)(GameObject*));
 
 #endif
