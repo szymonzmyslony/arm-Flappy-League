cf2f98fd34450b19d82b9edc2f11a06ed43842b6
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-09 19:57:02 +0100

    Used the new gObjs enums and fully implemented the scene setup.

--- src/extension/GameEdit.c
+++ src/extension/GameEdit.c
@@ -2,23 +2,64 @@
 
 void initMenu(void) {
   initSetup();
-
 }
 
 void initGame(void) {
+  // Setup score counters
+  gObjs[SCOREBOARD1] = initSquareObj(SCORE1_OFFSET_X, SCORE_OFFSET_Y,
+                                      SCORE_WIDTH, SCORE_HEIGHT, false);
+  gObjs[SCOREBOARD1]->v4.i = 0;
+  gObjs[SCOREBOARD2] = initSquareObj(SCORE2_OFFSET_X, SCORE_OFFSET_Y,
+                                      SCORE_WIDTH, SCORE_HEIGHT, false);
+  gObjs[SCOREBOARD2]->v4.i = 0;
+
   initSetup();
 }
 
 void initSetup(void) {
-  gObjs[0] = initCircleObj(32, 100, 300, 0,  0);
-  setSprite(gObjs[0], surf_datboi);
+  //Init Players
+  gObjs[PLAYER1] = initCircleObj(PLAYER_SIZE / 2, 100, 300, 0,  0);
+  setSprite(gObjs[PLAYER1], surf_bird1);
 
-  gObjs[1] = initCircleObj(32, screen->w - 100, 300, 0,  0);
-  setSprite(gObjs[1], surf_flappybird);
+  gObjs[PLAYER2] = initCircleObj(PLAYER_SIZE / 2, screen->w - 100, 300, 0,  0);
+  setSprite(gObjs[PLAYER2], surf_bird2);
 
-  gObjs[2] = initCircleObj(32, screen->w/2, 300, 0, 0);
-  setSprite(gObjs[2], surf_ball);
+  //Init Ball
+  gObjs[BALL] = initCircleObj(BALL_SIZE / 2, screen->w/2, 300, 0, 0);
+  setSprite(gObjs[BALL], surf_ball);
 
-  gObjs[9] = initTimerObj(UINT32_MAX, true, &updateTimerConstant, &applyAllGravity);
-  gObjs[10] = initTimerObj(UINT32_MAX, true, &updateTimerConstant, &applyAllAirResistance);
+  //Init Goals
+  gObjs[GOAL1] = initSquareObj(0, GOAL_OFFSET,
+                           GOAL_WIDTH, GOAL_HEIGHT, false);
+  setCollFunc(gObjs[GOAL1], &scorePlayer1);
+
+  gObjs[GOAL2] = initSquareObj(WINDOW_WIDTH - GOAL_WIDTH, GOAL_OFFSET,
+                           GOAL_WIDTH, GOAL_HEIGHT, false);
+  setCollFunc(gObjs[GOAL2], &scorePlayer2);
+
+  //Init Physics
+  gObjs[9] = initTimerObj(UINT32_MAX, true, &updateTimerConstant,
+                          &applyAllGravity);
+  gObjs[10] = initTimerObj(UINT32_MAX, true, &updateTimerConstant,
+                          &applyAllAirResistance);
+
+  //Init Countdown
+  gObjs[11] = initTimerObj(3000, true, &updateTimerAlarm,
+                          &playWhistleSound);
+}
+
+void scorePlayer1(GameObject *colObj) {
+  if(colObj == gObjs[BALL]) {
+    gObjs[SCOREBOARD1]->v4.i++;
+    initSetup();
+    Mix_PlayChannel(-1, sound_goal, 0);
+  }
+}
+
+void scorePlayer2(GameObject *colObj) {
+  if(colObj == gObjs[BALL]) {
+    gObjs[SCOREBOARD2]->v4.i++;
+    initSetup();
+    Mix_PlayChannel(-1, sound_goal, 0);
+  }
 }
