6930723963f8db66fa92fe15c99031f21e6bf63a
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-09 20:41:28 +0100

    Added function declarations.

--- src/extension/Control.h
+++ src/extension/Control.h
@@ -2,15 +2,13 @@
 #define CONTROL_H
 #include "Structs.h"
 #include "Tminus.h"
-#define upVelocity   10.0
-#define sideVelocity  5.0
 
-void moveLeft(GameObject *circObj);
-void moveRight(GameObject *circObj);
+void updateButtonsStatus(void);
+void updateButtonDownP1Left(void);
+void updateButtonDownP1Right(void);
+void updateButtonDownP2Left(void);
+void updateButtonDownP2Right(void);
 
-void check_left_first(void);
-void check_right_first(void);
-void check_left_second(void);
-void check_right_second(void);
+void handleButtonStatus(void);
 
 #endif
