3a2d860e6663daeff60876e3cbcf4241a7c0d226
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-09 20:40:56 +0100

    Renamed variables for clarity, fixed syntax mistakes.

--- src/extension/Control.c
+++ src/extension/Control.c
@@ -1,55 +1,33 @@
 #include "Control.h"
 
- bool go_left_first_player;
- bool go_right_first_player;
- bool go_right_second_player;
- bool go_left_second_player;
- 
- bool pressed_left_first_player;
- bool pressed_right_first_player;
- bool pressed_right_second_player;
- bool pressed_left_second_player;
- 
-
-
-
-#define CHECK(name, pinNO, isPressed, goPlayer)     \        
-  void name(void)                 \
-  {                        \
-   bool pin = digitalRead(pinNO)          \
-   if (pin&&!isPressed){                  \
-    isPressed = true;                     \
-	goPlayer = true; }                     \
-  else if(isPressed&&!pin){                \
-	  isPressed = false; }	               \
-}     
-
-CHECK(check_left_first, LeftFirstPlayer, pressed_left_first_player, go_left_first_player)
-CHECK(check_right_first, RightFirstPlayer, pressed_right_first_player, go_right_first_player)
-CHECK(check_left_second, LeftSecondPlayer, pressed_left_second_player, go_left_second_player)
-CHECK(check_right_second, RightSecondPlayer, pressed_right_second_player, go_right_second_player)
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-void moveLeft(GameObject *circObj) {
-  circObj->v2.vec.x = -sideVelocity;
-  circObj->v2.vec.y = -upVelocity;
+#define CHECK(name, pinNO, isPressed, goPlayer)     \
+void name(void) {                                   \
+  bool pin = digitalRead(pinNO);                    \
+  if(pin && !isPressed) {                           \
+    isPressed = true;                               \
+	  goPlayer = true;                                \
+  } else if(isPressed && !pin) {                    \
+	  isPressed = false;                              \
+  }	                                                \
 }
 
-void moveRight(GameObject *circObj) {
-  circObj->v2.vec.x = +sideVelocity;
-  circObj->v2.vec.y = -upVelocity;
+static bool buttonP1Left;
+static bool buttonP1Right;
+static bool buttonP2Left;
+static bool buttonP2Right;
+
+CHECK(updateButtonDownP1Left, LeftFirstPlayer, buttonP1Left,
+      buttonDownP1Left)
+CHECK(updateButtonDownP1Right, RightFirstPlayer, buttonP1Right,
+      buttonDownP1Right)
+CHECK(updateButtonDownP2Left, LeftSecondPlayer, buttonP2Left,
+      buttonDownP2Left)
+CHECK(updateButtonDownP2Right, RightSecondPlayer, buttonP2Right,
+      buttonDownP2Right)
+
+void updateButtonsStatus(void) {
+  updateButtonDownP1Left();
+  updateButtonDownP1Right();
+  updateButtonDownP2Left();
+  updateButtonDownP2Right();
 }
