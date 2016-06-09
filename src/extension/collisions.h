3012edbd9ae9bdc801be3129bd00ceb7da1643f2
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-09 19:55:27 +0100

    Added function declarations.

--- src/extension/collisions.h
+++ src/extension/collisions.h
@@ -14,6 +14,8 @@ enum colliderTypes {
 };
 
 void resolveCollision(Vector *v1, Vector *v2, int m1, int m2, float e);
+void resolveCollisionAdvanced(Vector *v1, Vector *v2, int m1, int m2,
+                              Vector *c1, Vector *c2, float e);
 bool circlesCollided(GameObject *c1, GameObject *c2);
 bool circleBoxCollided(GameObject *c, GameObject *b);
 bool circleNetCollided(GameObject *c, GameObject *b);
