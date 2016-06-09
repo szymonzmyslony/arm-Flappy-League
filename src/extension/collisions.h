37e93fdfbbadc0bf4e437aad5d2ef09c7c4096e6
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-09 15:45:33 +0100

    Added function declarations, and an extra collider type.

--- src/extension/collisions.h
+++ src/extension/collisions.h
@@ -6,12 +6,15 @@
 #include "Structs.h"
 
 enum colliderTypes {
-  COL_CIRCLE = 1, 
+  COL_CIRCLE = 1,
   COL_LINE   = 2,
   COL_NET    = 3,
+  COL_BOX    = 4
 };
 
 void resolveCollision(Vector *v1, Vector *v2, int m1, int m2, float e);
 bool circlesCollided(GameObject *c1, GameObject *c2);
+bool circleBoxCollided(GameObject *c, GameObject *b);
+bool circleNetCollided(GameObject *c, GameObject *b);
 
 #endif
