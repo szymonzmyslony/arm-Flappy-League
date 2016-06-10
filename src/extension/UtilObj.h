0d8e3777f8a6b69c841234c6d13178ed462f0568
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-10 20:29:10 +0100

    Split the TimerObj from UtilObj, moved some functions to GameEdit

--- src/extension/UtilObj.h
+++ src/extension/UtilObj.h
@@ -4,19 +4,7 @@
 #include "Common.h"
 #include "collisions.h"
 
-void timerObjAddTime(GameObject *timerObj);
-uint32_t timerObjElapsed(GameObject *timerObj);
-void timerObjStart(GameObject *timerObj);
-void timerObjStop(GameObject *timerObj);
-void updateTimerAlarm(GameObject *timerObj);
-void updateTimerRepeated(GameObject *timerObj);
-void updateTimerConstant(GameObject *timerObj);
-void initTimerObj(GameObject *timerObj, uint32_t duration, bool enable,
-  void (*updateMode)(GameObject*), void (*endAction)(void));
 void setSprite(GameObject *gameObj, SDL_Surface *sprite);
-void addAllVelocity(void);
-void applyAllGravity(void);
-void applyAllAirResistance(void);
 void playWhistleSound(void);
 void drawCentredObj(GameObject *collObj);
 
