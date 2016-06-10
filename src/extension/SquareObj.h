a5cd253fd6230f1127b6806994d751e55025b851
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-10 16:57:44 +0100

    Fixed function declarations.

--- src/extension/SquareObj.h
+++ src/extension/SquareObj.h
@@ -5,7 +5,8 @@
 #include "UtilObj.h"
 #include "Debug.h"
 
-GameObject *initSquareObj(float x, float y, float w, float h, bool isSolid);
+void *initSquareObj(GameObject *squareObj, float x, float y, float w, float h,
+  bool isSolid);
 void drawSquareObj(GameObject *squareObj);
 void setAsGoal(GameObject *scoreObj);
 void setCollFunc(GameObject *squareObj, void (*func)(GameObject*));
