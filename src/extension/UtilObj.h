c5159c53193a4fba663e2818775dcd6de473b071
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-12 04:46:06 +0100

    Added function declarations.

--- src/extension/UtilObj.h
+++ src/extension/UtilObj.h
@@ -1,11 +1,15 @@
 #ifndef UTILOBJ_H
 #define UTILOBJ_H
 
+#include <string.h>
 #include "Common.h"
 #include "collisions.h"
 
+void clearObj(GameObject *gameObj);
 void setSprite(GameObject *gameObj, SDL_Surface *sprite);
 void playWhistleSound(void);
 void drawCentredObj(GameObject *collObj);
+void setCollFunc(GameObject *collObj, void (*gFunc)(GameObject*));
+void playCollKickSound(GameObject *collObj);
 
 #endif
