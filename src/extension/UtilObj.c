2df2c2dbc02d6bd37b1a3fadfd1d4f13bf5cea5f
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-12 04:45:19 +0100

    Moved setting of collision functions here, added a clearObj function, added a kick sound function.

--- src/extension/UtilObj.c
+++ src/extension/UtilObj.c
@@ -1,11 +1,30 @@
 #include "UtilObj.h"
 
+#define collFunc collObj->v6.gFunc
+
+void clearObj(GameObject *gameObj) {
+  memset(gameObj, 0, sizeof(GameObject));
+}
+
 void setSprite(GameObject *gameObj, SDL_Surface *newSprite) {
   gameObj->sprite = newSprite;
 }
 
+void setCollFunc(GameObject *collObj, void (*gFunc)(GameObject*)) {
+  collFunc = gFunc;
+}
+
 void playWhistleSound(void) {
   Mix_PlayChannel(-1, sound_whistle, 0);
+}
+
+void playCollKickSound(GameObject *collObj) {
+  switch((int)(gObjs[BALL]->v1.vec.x) % 4) {
+    case 0: Mix_PlayChannel(-1, sound_kick1, 0); break;
+    case 1: Mix_PlayChannel(-1, sound_kick2, 0); break;
+    case 2: Mix_PlayChannel(-1, sound_kick3, 0); break;
+    case 3: Mix_PlayChannel(-1, sound_kick4, 0); break;
+  }
 }
 
 /** Pre: The GameObject must have v1 set to a position
