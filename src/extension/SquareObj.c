a5368d0794b7fa59dc89739274bc2346a3bc955d
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-12 04:39:53 +0100

    Cleared the object upon initialisation.

--- src/extension/SquareObj.c
+++ src/extension/SquareObj.c
@@ -4,8 +4,6 @@
 #define sy       squareObj->v1.vec.y
 #define sw       squareObj->v2.vec.x
 #define sh       squareObj->v2.vec.y
-#define collFunc squareObj->v3.func
-
 
 void animate(SDL_Surface *animationSource, int i, int j, int width, int height, int x, int y){
   SDL_Rect srcrect;
@@ -35,15 +33,12 @@ void drawSquareObj(GameObject *squareObj) {
   }
 }
 
-void setCollFunc(GameObject *squareObj, void (*func)(GameObject*)) {
-  squareObj->v3.gFunc = func;
-}
-
 /** An object which has a square collider. A sprite attached will be drawn
 * upon the top-left corner of the squareObj.
 */
 void initSquareObj(GameObject *squareObj, float x, float y, float w,
   float h, bool isSolid) {
+  clearObj(squareObj);
   squareObj->colliderType = isSolid? COL_BOX : COL_NET;
 
   sx = x;
