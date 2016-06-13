df783ac93b2873d6175dca517f1ee2a4bf11a82d
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-13 18:16:49 +0100

    Included GameChangers.

--- src/extension/CircleObj.h
+++ src/extension/CircleObj.h
@@ -1,10 +1,10 @@
 #ifndef CIRCLE_OBJ_H
 #define CIRCLE_OBJ_H
-
 #include "Common.h"
 #include "Debug.h"
 #include "collisions.h"
 #include "UtilObj.h"
+#include "GameChangers.h"
 
 void initCircleObj(GameObject *circObj, float r, float x, float y, float vx,
   float vy);
