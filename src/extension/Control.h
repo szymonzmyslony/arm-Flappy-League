cfd26988f1f3dd1ecaa4f20d82c0952c43b90eb5
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-13 18:16:12 +0100

    Moved pin enum here

--- src/extension/Control.h
+++ src/extension/Control.h
@@ -3,6 +3,15 @@
 #include "Common.h"
 #include "Main.h"
 
+/** GPIO pins
+*/
+enum Pins {
+  RightSecondPlayer = 12,
+  RightFirstPlayer = 1,
+  LeftFirstPlayer = 4,
+  LeftSecondPlayer = 13
+};
+
 void updateButtonsStatus(void);
 void updateButtonDownP1Left(void);
 void updateButtonDownP1Right(void);
