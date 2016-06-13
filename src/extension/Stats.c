54d4fd450a669d830c39a40d1c66bfd6037d3c69
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-13 02:57:49 +0100

    Started on a stats object

---
+++ src/extension/Stats.c
@@ -0,0 +1,21 @@
+#include "Stats.h"
+
+/** Stats are not drawn when this is called, use showStats to do so.
+*/
+void initStatsObj(GameObject *statsObj) {
+  clearObj(statsObj);
+
+  statsObj->update = &updateStats;
+}
+
+void showStats(GameObject *statsObj) {
+  statsObj->draw = drawStats;
+}
+
+void drawStats(GameObject *statsObj) {
+
+}
+
+void updateStats(GameObject *statsObj) {
+  
+}
---
+++ src/extension/Stats.h
@@ -0,0 +1,8 @@
+#ifndef STATS_H
+#define STATS_H
+#include "Common.h"
+#include "UtilObj.h"
+
+void drawStats(GameObject *statsObj);
+
+#endif
