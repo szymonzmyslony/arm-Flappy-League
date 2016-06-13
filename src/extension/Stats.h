9ed2381c17c5098f88ec0e7291a4dd1253feb8f6
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-13 03:09:36 +0100

    Added function declarations, made the defines globally visible.

--- src/extension/Stats.h
+++ src/extension/Stats.h
@@ -3,6 +3,14 @@
 #include "Common.h"
 #include "UtilObj.h"
 
+#define kicksP1 v1.vec.x
+#define kicksP2 v1.vec.y
+#define flapsP1 v2.vec.x
+#define flapsP2 v2.vec.y
+
+void initStatsObj(GameObject *statsObj);
+void showStats(GameObject *statsObj);
 void drawStats(GameObject *statsObj);
+void updateStats(GameObject *statsObj);
 
 #endif
