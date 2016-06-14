225bef58430a9c0b3ca318401e799cea55f6261c
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-14 03:24:21 +0100

    Started the drawStats function. Now draws numbers to the screen using enums defined in the header.

--- src/extension/Stats.c
+++ src/extension/Stats.c
@@ -13,7 +13,12 @@ void showStats(GameObject *statsObj) {
 }
 
 void drawStats(GameObject *statsObj) {
-  
+  // Draw Number of kicks for P1
+  drawNum(statsObj->v1.vec.x, STATS_KICK_OFFSET_X1, STATS_KICK_OFFSET_Y);
+  drawNum(statsObj->v2.vec.x, STATS_FLAP_OFFSET_X1, STATS_FLAP_OFFSET_Y);
+
+  drawNum(statsObj->v1.vec.y, STATS_FLAP_OFFSET_X2, STATS_KICK_OFFSET_Y);
+  drawNum(statsObj->v2.vec.y, STATS_FLAP_OFFSET_X2, STATS_FLAP_OFFSET_Y);
 }
 
 void updateStats(GameObject *statsObj) {
--- src/extension/Stats.h
+++ src/extension/Stats.h
@@ -8,6 +8,19 @@
 #define flapsP1 v2.vec.x
 #define flapsP2 v2.vec.y
 
+enum statsSizesAndPositions {
+  STATS_ENTRY_GAP = 24,
+
+  STATS_KICK_OFFSET_X1 = WINDOW_WIDTH * 1 / 4 - NUM_3WIDTH / 2,
+  STATS_FLAP_OFFSET_X1 = STATS_KICK_OFFSET_X1,
+
+  STATS_KICK_OFFSET_X2 = WINDOW_WIDTH * 3 / 4 - NUM_3WIDTH / 2,
+  STATS_FLAP_OFFSET_X2 = STATS_KICK_OFFSET_X2,
+
+  STATS_KICK_OFFSET_Y = WINDOW_HEIGHT / 2,
+  STATS_FLAP_OFFSET_Y = STATS_KICK_OFFSET_Y + NUM_HEIGHT + STATS_ENTRY_GAP,
+};
+
 void initStatsObj(GameObject *statsObj);
 void showStats(GameObject *statsObj);
 void drawStats(GameObject *statsObj);
