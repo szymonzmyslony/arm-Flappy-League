a8b655a332f1ae34768aa03ce929116e479e8fcb
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2016-06-14 03:19:57 +0100

    Moved animate to UtilsObj

--- src/extension/SquareObj.c
+++ src/extension/SquareObj.c
@@ -5,23 +5,6 @@
 #define sw       squareObj->v2.vec.x
 #define sh       squareObj->v2.vec.y
 
-void animate(SDL_Surface *animationSource, int i, int j, int width, int height,
-             int x, int y){
-  SDL_Rect srcrect;
-  int startx = i * width;
-  int starty = j * height;
-  srcrect.x = startx;
-  srcrect.y = starty;
-  srcrect.w = width;
-  srcrect.h = height;
-  SDL_Rect destrec;
-  destrec.x = x;
-  destrec.y = y;
-  destrec.w = width;
-  destrec.h = height;
-  SDL_BlitSurface(animationSource, &srcrect, screen, &destrec);
-}
-
 void drawSquareObj(GameObject *squareObj) {
   if(squareObj->sprite != NULL) {
     SDL_Rect pos = { .x = sx,
